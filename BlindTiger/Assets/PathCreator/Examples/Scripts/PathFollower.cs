1364cad6af77039c220678c9272013ff48d471b9
madcpuscientist <madcpuscientist@gmail.com>
2020-04-26 16:37:57 -0400

    balanced the track so the paths are relatively equal. cars now collide and spin out

--- BlindTiger/Assets/PathCreator/Examples/Scripts/PathFollower.cs
+++ BlindTiger/Assets/PathCreator/Examples/Scripts/PathFollower.cs
@@ -6,26 +6,43 @@ namespace PathCreation.Examples
     // Depending on the end of path instruction, will either loop, reverse, or stop at the end of the path.
     public class PathFollower : MonoBehaviour
     {
+        public const float DEFAULT_DRIVING_SPEED = 100;
+        public const float MIN_DRIVING_SPEED = 80;
+        public const float MAX_DRIVING_SPEED = 120;
+        public const int SPIN_FACTOR = 18;
         public PathCreator pathCreator;
         public EndOfPathInstruction endOfPathInstruction;
-        public float speed = 5;
+        public int spinAngle = 0;
+        public float speed;
         float distanceTravelled;
+        bool crashing = false;
 
         void Start() {
             if (pathCreator != null)
             {
                 // Subscribed to the pathUpdated event so that we're notified if the path changes during the game
                 pathCreator.pathUpdated += OnPathChanged;
-            }
+                speed = DEFAULT_DRIVING_SPEED;
+    }
         }
 
         void Update()
         {
             if (pathCreator != null)
             {
-                distanceTravelled += speed * Time.deltaTime;
-                transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled, endOfPathInstruction);
-                transform.rotation = pathCreator.path.GetRotationAtDistance(distanceTravelled, endOfPathInstruction);
+                if (!crashing)
+                {
+                    speed = Random.Range(MIN_DRIVING_SPEED, MAX_DRIVING_SPEED);
+                    Debug.Log(speed);
+                    distanceTravelled += speed * Time.deltaTime;
+                    transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled, endOfPathInstruction);
+                    transform.rotation = pathCreator.path.GetRotationAtDistance(distanceTravelled, endOfPathInstruction);
+                }
+                
+                if (crashing)
+                {
+                    Spin();
+                }
             }
         }
 
@@ -33,6 +50,37 @@ namespace PathCreation.Examples
         // is as close as possible to its position on the old path
         void OnPathChanged() {
             distanceTravelled = pathCreator.path.GetClosestDistanceAlongPath(transform.position);
+        }
+
+
+        // Adding for simplicity purposes
+        void OnCollisionEnter(Collision aCollidingObject)
+        {
+            if (aCollidingObject.collider.tag == "car")
+            {
+                crashing = true;
+                Spin();
+            }
+        }
+
+        void Spin()
+        {
+            
+            if (speed > 0)
+            {
+                spinAngle += SPIN_FACTOR;
+                speed -= 5;
+                distanceTravelled += speed * Time.deltaTime;
+                transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled, endOfPathInstruction);
+                transform.rotation = Quaternion.AngleAxis(spinAngle, Vector3.up);
+            }
+
+            else if (speed <= 0)
+            {
+                speed = DEFAULT_DRIVING_SPEED;
+                spinAngle = 0;
+                crashing = false;
+            }
         }
     }
 }
